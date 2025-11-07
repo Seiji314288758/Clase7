dbdeb6df47c7a959a4e58cca199e077e2d5f7731
Seiji314288758 <nietoseiji@gmail.com>
2025-11-07 14:06:26 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,3 +1,38 @@
 /*Victor Seiji Nieto Morita
-clase 11 ejercicio 1*/
+clase 7 ejercicio 1*/
 
+#include <stdio.h>
+#define dim 20
+
+void main()
+{
+ int arr[dim], num, i, j;
+
+  i = 0;
+do
+  {
+   printf("\nDame un numero entero: ");
+   scanf("%d", &num);
+
+   if(num != 0) 
+   {
+    i = i + 1; 
+    arr[dim - i] = num;
+   }  
+  }
+ while ((num != 0) && (i < 20));
+
+ if(i > 0)
+  {
+   printf("Los valores introducidos son: \n");
+   for(j = dim - 1; j < dim - i; j --) 
+     {
+      printf("%d\t", arr[j]);
+     }
+  } 
+ else
+ { 
+  printf("No ha introducido ningun valor\n");
+ }  
+ printf("\n");
+}
